02291e515267f5ca0f1c37b5c83944e4b921017e
EleonoreFauchet <68398218+EleonoreFauchet@users.noreply.github.com>
2022-01-27 13:33:46 +0100

    Update TP_ITS.py

--- Projet10-VSL/TP_simulation/TP_ITS.py
+++ Projet10-VSL/TP_simulation/TP_ITS.py
@@ -106,54 +106,7 @@ def activate_VSL_system(position1, position2, time1, time2, limitated_veh_list, 
     
     return limitated_veh_list
 
-## def activate VSL system with only 2 CV side-by-side
-def find_two_veh(position1, speed_limit, position2, veh_lane0, veh_lane1):
-    """ find the two vehicles which will be limitated by the VSL system, one one each lane and the closest ones to position1"""
-    potential_vehicles = []
         
-    running_veh = traci.vehicle.getIDList()
-            
-    for veh in running_veh : 
-        if traci.vehicle.getTypeID(veh) == 'CV':
-            position = round(traci.vehicle.getPosition(veh)[0],1)
-            if position1 <= position <= position2:
-                potential_vehicles += [[veh, position, traci.vehicle.getLaneIndex(veh)]]
-        
-        print(potential_vehicles) 
-        potential_vehicles = sorted(potential_vehicles, key=lambda item:item[1]) #dans l'ordre de leur dernière position de la plus petit à la plus grande 
-        print(potential_vehicles) 
-
-        for veh in potential_vehicles: 
-            if  len(veh_lane0) <= 0 or len(veh_lane1) <= 0:
-                if len(veh_lane0) == 0 and veh[2] == 0:
-                    veh_lane0 += [veh]
-                if len(veh_lane1) == 0 and veh[2] == 1:
-                    veh_lane1 += [veh]
-    print(veh_lane0, veh_lane1)
-
-    traci.vehicle.setMaxSpeed(veh_lane0[0][0], speed_limit)
-    traci.vehicle.setMaxSpeed(veh_lane1[0][0], speed_limit)
-
-    return veh_lane0, veh_lane1
-        
-
-def two_vehicles_limitated(position1, position2, time1, step, speed_limit, veh_lane0, veh_lane1):
-    """ apply VSL system to only two vehicles side-by-side"""
-    if step == time1:
-        veh_lane0, veh_lane1 = find_two_veh(position1, speed_limit, position2, veh_lane0, veh_lane1)
-    
-    
-    if step > time1:
-        if len(veh_lane0) > 0:
-            if traci.vehicle.getPosition(veh_lane0[0][0])[0] > position2:
-                traci.vehicle.setMaxSpeed(veh_lane0[0][0], 33.11)
-                veh_lane0 = []
-
-        if len(veh_lane1) > 0:
-            if traci.vehicle.getPosition(veh_lane1[0][0])[0] > position2:
-                traci.vehicle.setMaxSpeed(veh_lane1[0][0], 33.11)
-                veh_lane1 = []
-    return veh_lane1, veh_lane0
         
 ## def lane change system
 def activate_lane_changing(position1, position2, time1, time2, limitated_veh_list, step):
@@ -196,10 +149,6 @@ def run():
         ##limitated_veh_list = activate_VSL_system(2000, 5000, 600, 1200, limitated_veh_list, SPEED_LIMIT, step)
         ##print('number of limitated vehicles', len(limitated_veh_list))
         
-
-        ### Uncomment this part in question 2.2
-        ### System to limitate speed of 2 vehicles side-by-side between 2000 and 5000m from 5min until they reach position = 5000m
-        ##veh_lane1, veh_lane0 = two_vehicles_limitated(2000, 5000, 600, step, SPEED_LIMIT, veh_lane0, veh_lane1)
 
         ### Uncomment this part in question 2.2
         ### System to activate lane changing to all Connected Vehicles between 2000 and 5000m between 5 and 10min 
